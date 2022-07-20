b3643c2850f769b2c108e8080b7ff00d393d8b8a
bnbnac <bnbnac@naver.com>
2022-07-20 17:02:21 +0900

    writheFragment id에는 먼저 캐시가 어떻게 생겼는지 본다. 이 경우 Movie:12345 이런 구조였음 fragment에는 캐시에 바꾸고싶은 유형을 쓴다.(캐시에 올리고 싶은 유형?) data에는 바꾸고싶은 내용을 쓴다. fragment에 썼던것만 바뀐다.

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -1,3 +1,101 @@
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router";
+import styled from "styled-components";
+
+const GET_MOVIE = gql`
+  query getMovie($movieId: String!) {
+    movie(id: $movieId) {
+      id
+      title
+      medium_cover_image
+      rating
+      isLiked @client
+    }
+  }
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+  width: 50%;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 28px;
+`;
+
+const Image = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
+  background-image: url(${(props) => props.bg});
+  background-size: cover;
+  background-position: center center;
+  border-radius: 7px;
+`;
+
 export default function Movie() {
-  return <div>This is Movie detail</div>;
+  const { id } = useParams();
+  const {
+    data,
+    loading,
+    client: { cache },
+  } = useQuery(GET_MOVIE, {
+    variables: {
+      movieId: id,
+    },
+  });
+
+  // writheFragment
+  // id에는 먼저 캐시가 어떻게 생겼는지 본다.
+  // 이 경우 Movie:12345 이런 구조였음
+  // fragment에는 캐시에 바꾸고싶은 유형을 쓴다.(캐시에 올리고 싶은 유형?)
+  // data에는 바꾸고싶은 내용을 쓴다. fragment에 썼던것만 바뀐다.
+
+  const onClick = () => {
+    cache.writeFragment({
+      id: `Movie:${id}`,
+      fragment: gql`
+        fragment MovieFragment on Movie {
+          isLiked
+        }
+      `,
+      data: {
+        isLiked: !data.movie.isLiked,
+      },
+    });
+    console.log(data.movie.isLiked);
+  };
+
+  return (
+    <Container>
+      <Column>
+        <Title>{loading ? "Loading..." : `${data.movie?.title}`}</Title>
+        <Subtitle>⭐️{data?.movie?.rating}</Subtitle>
+        <button onClick={onClick}>
+          {data?.movie?.isLiked ? "Unlike" : "Like"}
+        </button>
+      </Column>
+      <Image bg={data?.movie?.medium_cover_image} />
+    </Container>
+  );
 }
