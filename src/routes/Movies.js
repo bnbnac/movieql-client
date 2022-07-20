8e1f2a6cfa759ced086adbb13c2ccdf120b39aa0
bnbnac <bnbnac@naver.com>
2022-07-20 11:34:30 +0900

    element connection with react and gql

--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,3 +1,29 @@
+import { gql, useApolloClient } from "@apollo/client";
+import { useEffect, useState } from "react";
+
 export default function Movies() {
-  return <div>This is List of Movies</div>;
+  const client = useApolloClient();
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    client
+      .query({
+        query: gql`
+          {
+            allMovies {
+              title
+            }
+          }
+        `,
+      })
+      .then((result) => setMovies(result.data.allMovies));
+  }, [client]);
+  return (
+    <div>
+      {movies.map((movie) => (
+        <li key={movie.id}>{movie.title}</li>
+      ))}
+    </div>
+  );
 }
+// 1 dependency client
+// 2 apolloprovider
